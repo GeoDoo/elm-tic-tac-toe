678a751a8dd3c8603dfe239b56089e3286e7ea69
Georgios Karametas <geometas@hotmail.com>
2021-12-09 21:52:27 +0000

    remove debug html

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,7 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, div, text)
+import Html exposing (Html, div)
 import Html.Attributes exposing (class)
 
 
@@ -64,5 +64,4 @@ view model =
             , div [ class "cell" ] []
             , div [ class "cell" ] []
             ]
-        , div [] [ text (Debug.toString model) ]
         ]
