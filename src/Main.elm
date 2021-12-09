98482fff42e60f3e868524904ca466c22cad5cf9
Georgios Karametas <geometas@hotmail.com>
2021-12-09 21:19:11 +0000

    first take

---
+++ src/Main.elm
@@ -0,0 +1,68 @@
+module Main exposing (..)
+
+import Browser
+import Html exposing (Html, div, text)
+import Html.Attributes exposing (class)
+
+
+
+-- MAIN
+
+
+main : Program () Model msg
+main =
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    List Mark
+
+
+init : Model
+init =
+    [ Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing ]
+
+
+type Mark
+    = X
+    | O
+    | Nothing
+
+
+
+-- UPDATE
+
+
+type Msg
+    = None String
+
+
+update : msg -> Model -> Model
+update _ model =
+    model
+
+
+
+-- VIEW
+
+
+view : Model -> Html msg
+view model =
+    div [ class "root" ]
+        [ div [ class "board" ]
+            [ div [ class "cell" ] []
+            , div [ class "cell" ] []
+            , div [ class "cell" ] []
+            , div [ class "cell" ] []
+            , div [ class "cell" ] []
+            , div [ class "cell" ] []
+            , div [ class "cell" ] []
+            , div [ class "cell" ] []
+            , div [ class "cell" ] []
+            ]
+        , div [] [ text (Debug.toString model) ]
+        ]
