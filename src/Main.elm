ac9b9bdde61e5c90771fa78db65f1670dbae0dc4
Georgios Karametas <geometas@hotmail.com>
2021-12-29 17:32:44 +0000

    change model and markup

--- src/Main.elm
+++ src/Main.elm
@@ -1,15 +1,16 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, div)
+import Html exposing (Html, button, div, li, text, ul)
 import Html.Attributes exposing (class)
+import Html.Events exposing (onClick)
 
 
 
 -- MAIN
 
 
-main : Program () Model msg
+main : Program () Model Msg
 main =
     Browser.sandbox { init = init, update = update, view = view }
 
@@ -19,18 +20,17 @@ main =
 
 
 type alias Model =
-    List Mark
-
-
-init : Model
-init =
-    [ Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing ]
+    List ( Int, String )
 
 
 type Mark
     = X
     | O
-    | Nothing
+
+
+init : Model
+init =
+    List.indexedMap Tuple.pair [ "", "", "", "", "", "", "", "", "" ]
 
 
 
@@ -38,30 +38,41 @@ type Mark
 
 
 type Msg
-    = None String
+    = AddMark Int
 
 
-update : msg -> Model -> Model
-update _ model =
-    model
+updateEntry : Int -> ( Int, String ) -> ( Int, String )
+updateEntry index tuple =
+    if index == Tuple.first tuple then
+        ( index, "X" )
+
+    else
+        tuple
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        AddMark index ->
+            List.map (\x -> updateEntry index x) model
 
 
 
 -- VIEW
 
 
-view : Model -> Html msg
-view _ =
+view : Model -> Html Msg
+view model =
     div [ class "root" ]
-        [ div [ class "board" ]
-            [ div [ class "cell" ] []
-            , div [ class "cell" ] []
-            , div [ class "cell" ] []
-            , div [ class "cell" ] []
-            , div [ class "cell" ] []
-            , div [ class "cell" ] []
-            , div [ class "cell" ] []
-            , div [ class "cell" ] []
-            , div [ class "cell" ] []
+        [ ul [ class "board ul" ]
+            [ li [ class "cell" ] [ button [ class "button", onClick (AddMark 0) ] [ text "" ] ]
+            , li [ class "cell" ] [ button [ class "button", onClick (AddMark 1) ] [ text "" ] ]
+            , li [ class "cell" ] [ button [ class "button", onClick (AddMark 2) ] [ text "" ] ]
+            , li [ class "cell" ] [ button [ class "button", onClick (AddMark 3) ] [ text "" ] ]
+            , li [ class "cell" ] [ button [ class "button", onClick (AddMark 4) ] [ text "" ] ]
+            , li [ class "cell" ] [ button [ class "button", onClick (AddMark 5) ] [ text "" ] ]
+            , li [ class "cell" ] [ button [ class "button", onClick (AddMark 6) ] [ text "" ] ]
+            , li [ class "cell" ] [ button [ class "button", onClick (AddMark 7) ] [ text "" ] ]
+            , li [ class "cell" ] [ button [ class "button", onClick (AddMark 8) ] [ text "" ] ]
             ]
         ]
